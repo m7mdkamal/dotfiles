461c329247b85018ab1a9318983049b09c96df69
Kamal <mohamed.kamel@eventtus.com>
2019-09-14 20:39:33 +0200

    Fix spotify new line

--- bin/spotify.py
+++ bin/spotify.py
@@ -6,14 +6,14 @@ cmd = os.popen('playerctl -p spotify status').read()
 cmd = cmd.split('\n')
 cmd = cmd[0]
 
-meta_artist = os.popen('playerctl -p spotify metadata xesam:artist').read()
-meta_title = os.popen('playerctl -p spotify metadata xesam:title').read()
+meta_artist = os.popen('playerctl -p spotify metadata xesam:artist').read().rstrip()
+meta_title = os.popen('playerctl -p spotify metadata xesam:title').read().rstrip()
 
 if cmd == "Playing":
-    print(meta_title + ' - ' + meta_artist)
+    print(meta_title + ' | ' + meta_artist)
 
 elif cmd == 'Paused':
-    print(meta_title + ' - ' + meta_artist)
+    print(meta_title + ' | ' + meta_artist)
 
 else:
     print('no music playing')
