e31ceafd9247c1a086baf07ba59a76a8ea118528
Kamal <mohammed.kamal@swvl.com>
2020-05-03 00:26:11 +0200

    Update dotfiles

--- import.sh
+++ import.sh
@@ -12,8 +12,6 @@ cp ~/.config/polybar/launch.sh ./polybar
 # bin files
 cp ~/bin/bat.sh ./bin/
 cp ~/bin/vol.sh ./bin/
-cp ~/bin/kubernetes.sh ./bin/
-cp ~/bin/spotify.py ./bin/
 
 # i3
 cp ~/.config/i3/config ./i3/config
